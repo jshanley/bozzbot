80c329635b6a4e142a53079a00629c85f32b3ad1
John Shanley <john.michael.shanley@gmail.com>
2015-11-29 16:03:02 -0500

    if this works...

--- scripts/gifme.js
+++ scripts/gifme.js
@@ -3,31 +3,71 @@
 
 'use strict';
 
+let path = require('path');
+
 let slackToken = process.env.HUBOT_SLACK_TOKEN;
+let gifAppUrl = process.env.GIFHEAD_APPLICATION_URL;
 
 module.exports = (robot) => {
 
-  robot.respond(/emoji url for (.+)/i, (res) => {
-    let emojiName = res.match[1];
-    emojiName = emojiName.replace(/\:/g, '').trim()
-    res.http(`https://slack.com/api/emoji.list?token=${slackToken}`)
-      .get()(function(err, r, body) {
-        console.log('err', err);
-        console.log('body', body);
-        var parsedBody = JSON.parse(body);
-        let emoji = parsedBody.emoji;
-        console.log('emoji', emoji);
-        let emojiUrl = emoji[emojiName];
-        if (emojiUrl) {
-          if (emojiUrl.slice(0,6) === 'alias:') {
-            res.send(emoji[emojiUrl.slice(6)])
+  function getEmojiUrl(emojiName) {
+    let promise = new Promise(function(resolve, reject) {
+      robot.http(`https://slack.com/api/emoji.list?token=${slackToken}`)
+        .get()(function(err, res, body) {
+          if (err) {
+            reject('Error looking up emoji URL.');
           } else {
-            res.send(emojiUrl)
+            let parsedBody = JSON.parse(body);
+            let emoji = parsedBody.emoji;
+            let emojiUrl = emoji[emojiName];
+            if (emojiUrl) {
+              if (emojiUrl.slice(0,6) === 'alias:') {
+                resolve(emoji[emojiUrl.slice(6)])
+              } else {
+                resolve(emojiUrl)
+              }
+            } else {
+              reject(`No emoji found for name: ${emojiName}`)
+            }
           }
-        } else {
-          res.send(`No emoji found for name: ${emojiName}`)
-        }
+        })
+    })
+    return promise;
+  }
+
+  function getGifUrl(gifName, headUrl) {
+    let promise = new Promise(function(resolve, reject) {
+      let postData = JSON.stringify({
+        head_url: headUrl
+      });
+      robot.http(`${gifAppUrl}/api/${gifName}/annotate`)
+        .header('Content-Type', 'application/json')
+        .header('Accept', 'application/json')
+        .post(postData)(function(err, res, body) {
+          if (err) {
+            reject('Error processing gif.')
+          } else {
+            let parsedBody = JSON.parse(body);
+            let gifPath = parsedBody.gif;
+            resolve(path.join(gifAppUrl, gifPath));
+          }
+        })
+    })
+    return promise;
+  }
+
+  robot.respond(/gif (\S+) (\S+)/i, (res) => {
+    let gifName = res.match[1];
+    let emoji = res.match[2];
+    let emojiName = emoji.replace(/\:/g, '').trim()
+    getEmojiUrl(emojiName)
+      .then((emojiUrl) => {
+        res.send('Ok, hang on a sec...');
+        getGifUrl(gifName, emojiUrl)
+          .then((gifUrl) => res.send(gifUrl))
+          .catch((msg) => res.send(msg))
       })
+      .catch((msg) => res.send(msg))
   })
 
 }
